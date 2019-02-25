ae7b861eeefa487e224633bce8cd9c650c15299b
giturtle <851520508@qq.com>
2019-02-25 22:46:51 +0800

    顺序表

--- test4/test4/code.c
+++ test4/test4/code.c
@@ -1,6 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <assert.h>
-#include <stdbool.h>
+
+int main() {
+
+	system("pause");
+	return 0;
+}
