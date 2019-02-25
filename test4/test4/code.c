1b49fb13c3cf1eebea1ccc5460c2385b12b0608d
giturtle <851520508@qq.com>
2019-02-25 23:04:10 +0800

    删除链表中给定值的所有节点

--- test4/test4/code.c
+++ test4/test4/code.c
@@ -2,8 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
 
-	system("pause");
-	return 0;
+//删除链表中等于给定值 val 的所有节点。
+//示例 :
+//输入: 1->2->6->3->4->5->6, val = 6
+//输出: 1->2->3->4->5
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+ 
+struct ListNode* removeElements(struct ListNode* head, int val) {
+	if (head == NULL) {
+		return NULL;
+	}
+	struct ListNode *prev = head;
+	struct ListNode *cur = head->next;
+	while (cur != NULL) {
+		if (cur->val != val) {
+			prev = cur;
+		}
+		else {
+			prev->next = cur->next;
+			free(cur);
+		}
+		cur = prev->next;
+	}
+	struct ListNode *NewHead = head;
+	if (head->val == val) {
+		NewHead = head->next;
+		free(head);
+	}
+	return NewHead;
 }
