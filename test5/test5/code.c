c4397280fed794a80aaafb3d3eb98a2d4bbbd719
giturtle <851520508@qq.com>
2019-02-26 21:57:06 +0800

    反转链表

---
+++ test5/test5/code.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+//反转一个链表
+//输入: 1->2->3->4->5->NULL
+//输出: 5->4->3->2->1->NULL
+
+struct ListNode {
+	int val;
+	struct ListNode *next;
+};
+
+struct ListNode* reverseList(struct ListNode* head) {
+	if (!head) {		//处理空链表
+		return head;
+	}
+	struct ListNode* p = head, *newHead = head;
+	while (head->next) {		
+		//交换两结点指针
+		p = head->next;
+		head->next = p->next;
+		p->next = newHead;
+		newHead = p;
+	}
+	return newHead;
+}
