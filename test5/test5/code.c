8fc161f91889584829ada4b4d2b23c7849e85293
giturtle <851520508@qq.com>
2019-02-27 19:27:42 +0800

    反转链表

--- test5/test5/code.c
+++ test5/test5/code.c
@@ -11,19 +11,20 @@ struct ListNode {
 	struct ListNode *next;
 };
 
-//方案一
+
 struct ListNode* reverseList(struct ListNode* head) {
-	if (!head || !head->next) {		//处理空链表
+	if (!head || !head->next) {		//处理空链表及单结点链表
 		return head;
 	}
-	struct ListNode* p = head, *newHead = head;
-	while (head->next) {		
-		//交换两结点指针
-		p = head->next;
-		head->next = p->next;
-		p->next = newHead;
-		newHead = p;
+	struct ListNode* pPre = NULL;
+	struct ListNode* pCur = head;
+	struct ListNode* pNext = NULL;
+	while (pCur) {
+		pNext = pCur->next;		//把当前结点的next保存下来
+		pCur->next = pPre;
+		pPre = pCur;
+		pCur = pNext;
 	}
-	return newHead;
+	return pPre;
 }
 
